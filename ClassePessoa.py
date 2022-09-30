9f118440519b3d5b67dff33f9097354021e7aada
Ana Carolina Schmitz da Silva <101817785+anacschmitz@users.noreply.github.com>
2022-09-30 15:17:17 -0300

    Add files via upload

---
+++ ClassePessoa.py
@@ -0,0 +1,44 @@
+class Pessoa():
+  def __init__(self, nome, cpf, matricula, tipo, senha):
+    self.nome = nome
+    self.cpf = cpf
+    self.matricula = matricula
+    self.tipo = tipo
+    self.senha = senha
+  
+  def login(self,matriculaValidar, senhaValidar):
+    self.matriculaValidar = matriculaValidar
+    self.senhaValidar = senhaValidar
+    ###CRUD PARA VALIDAR O LOGIN
+
+class Admin(Pessoa):
+  def __init__(self, nome, cpf, matricula, tipo, senha):
+    super().__init__(nome, cpf, matricula, tipo, senha)
+
+  def cadastrarUser(self):
+    print(self.nome) ##INSERIR CRUD AQUI
+  def deletarUser(self):
+    print(self.nome) ##INSERIR CRUD AQUI
+  def atualizarUser(self):
+    print(self.nome) ##INSERIR CRUD AQUI
+  def consultarUser(self):
+    print(self.nome) ##INSERIR CRUD AQUI
+  
+class User(Pessoa):
+  def __init__(self, nome, cpf, matricula, tipo, senha):
+    super().__init__(nome, cpf, matricula, tipo, senha)
+  
+  def adicionarProduto(self):
+      self.produto = input(int("Insira o código (ID) do produto:\n"))
+      listaVenda = []
+      listaVenda.append(self.produto)
+      return listaVenda
+
+  def consultarVendas(self):
+    print("Diego fará um CRUD")
+
+  def consultarVendas(self):
+    print("Diego fará um CRUD")
+  
+
+  
---
+++ chamarMenu.py
@@ -0,0 +1,5 @@
+class ChamarMenu:
+  def __init__(self):
+    print("MENU INICIAL")
+    opcao = input(int(("Escolha a opção desejada:\n1 - LOGIN \n2 - Consultar Produto \n3 - SAIR")))
+    return opcao
